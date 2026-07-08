ef1ad3e4d59b67044ce91488dfdcc2e5a47c2143
youri <youriemmanue@gmail.com>
2026-07-08 14:23:55 +0200

    Move snake automatically

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -12,6 +12,7 @@
 #define GRID_W (SCREEN_WIDTH / CELL)
 #define GRID_H (SCREEN_HEIGHT / CELL)
 #define MAX_LEN (GRID_W * GRID_H)
+#define STEP_MS 120
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1, 100000, 100000);
 
@@ -20,12 +21,60 @@ struct Point { int8_t x; int8_t y; };
 Point snake[MAX_LEN];
 int snakeLen;
 Point food;
+int8_t dirX;
+int8_t dirY;
+unsigned long lastStep;
+
+void placeFood() {
+  bool onSnake;
+  do {
+    food.x = random(GRID_W);
+    food.y = random(GRID_H);
+    onSnake = false;
+    for (int i = 0; i < snakeLen; i++)
+      if (snake[i].x == food.x && snake[i].y == food.y) { onSnake = true; break; }
+  } while (onSnake);
+}
+
+void resetGame() {
+  snakeLen = 3;
+  snake[0] = { GRID_W / 2, GRID_H / 2 };
+  snake[1] = { GRID_W / 2 - 1, GRID_H / 2 };
+  snake[2] = { GRID_W / 2 - 2, GRID_H / 2 };
+  dirX = 1;
+  dirY = 0;
+  placeFood();
+}
+
+void step() {
+  int nx = snake[0].x + dirX;
+  int ny = snake[0].y + dirY;
+  if (nx < 0) nx = GRID_W - 1;
+  else if (nx >= GRID_W) nx = 0;
+  if (ny < 0) ny = GRID_H - 1;
+  else if (ny >= GRID_H) ny = 0;
+  Point next = { (int8_t)nx, (int8_t)ny };
+
+  for (int i = 0; i < snakeLen - 1; i++)
+    if (snake[i].x == next.x && snake[i].y == next.y) {
+      resetGame();
+      return;
+    }
+
+  bool eat = (next.x == food.x && next.y == food.y);
+
+  for (int i = snakeLen; i > 0; i--) snake[i] = snake[i - 1];
+  snake[0] = next;
+  if (eat) {
+    if (snakeLen < MAX_LEN) snakeLen++;
+    placeFood();
+  }
+
+  if (snakeLen >= MAX_LEN) resetGame();
+}
 
 void draw() {
   display.clearDisplay();
-  for (int x = 0; x < SCREEN_WIDTH; x += CELL)
-    for (int y = 0; y < SCREEN_HEIGHT; y += CELL)
-      display.drawPixel(x, y, SSD1306_WHITE);
   display.drawRect(food.x * CELL, food.y * CELL, CELL, CELL, SSD1306_WHITE);
   for (int i = 0; i < snakeLen; i++)
     display.fillRect(snake[i].x * CELL, snake[i].y * CELL, CELL, CELL, SSD1306_WHITE);
@@ -37,16 +86,15 @@ void setup() {
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR, true, false)) {
     for (;;) ;
   }
-
-  snakeLen = 3;
-  snake[0] = { GRID_W / 2, GRID_H / 2 };
-  snake[1] = { GRID_W / 2 - 1, GRID_H / 2 };
-  snake[2] = { GRID_W / 2 - 2, GRID_H / 2 };
-
-  food = { GRID_W / 2 + 5, GRID_H / 2 };
-
-  draw();
+  randomSeed(esp_random());
+  resetGame();
+  lastStep = millis();
 }
 
 void loop() {
+  if (millis() - lastStep >= STEP_MS) {
+    lastStep = millis();
+    step();
+    draw();
+  }
 }
